96cd6dbdecc4c66a7eb790e98dfef394ee7bbf26
J-Moessmer <257735484+J-Moessmer@users.noreply.github.com>
2026-08-07 11:54:35 +0200

    feat: add datainsert.sql script for practicing DML operations including inserts, updates, and deletes

--- sql/DML/datainsert.sql
+++ sql/DML/datainsert.sql
@@ -13,9 +13,48 @@ INSERT INTO kunde (id, vorname, name)
 VALUES (2, N'Mia' , N'Musterfrau')
 --verkürzte schreibweise ohne datum
 
-SELECT *
-FROM kunde;
+
 
 --neue datensätze
 INSERT INTO bestellung
 VALUES ('20260807', 123.45, 1);
+
+
+SELECT *
+FROM kunde;
+
+
+
+--daten updates
+
+UPDATE kunde
+SET vorname = 'maximilian'
+WHERE vorname = 'Max'
+
+SELECT *
+FROM kunde;
+
+
+UPDATE kunde
+SET vorname = 'max'
+WHERE vorname = 'maximilian'
+
+SELECT *
+FROM kunde;
+SELECT *
+FROM bestellung;
+
+
+
+--löschen
+INSERT INTO kunde
+VALUES (3, N'Liam', N'löschmichmann', '19900404');
+
+SELECT *
+FROM kunde;
+
+DELETE FROM kunde
+WHERE id = 3; -- ohne where werden ale daten gelöscht
+
+SELECT *
+FROM kunde;
