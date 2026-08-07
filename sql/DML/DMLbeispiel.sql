52ef6a741b3f36f110471bd6ea7c1b55b45bfe10
J-Moessmer <257735484+J-Moessmer@users.noreply.github.com>
2026-08-07 11:08:46 +0200

    feat: add DML example and data insertion scripts

---
+++ sql/DML/DMLbeispiel.sql
@@ -0,0 +1,31 @@
+USE master
+DROP DATABASE IF EXISTS DMLBeispiel
+
+CREATE DATABASE DMLBeispiel
+GO
+
+USE DMLBeispiel
+GO
+
+CREATE TABLE kunde (
+    id INT,
+    vorname NVARCHAR(50) NOT NULL,
+    name NVARCHAR(59)  NOT NULL,
+    gebDat DATE NULL,
+    CONSTRAINT pk_kunde 
+        PRIMARY KEY (id)
+);
+
+CREATE TABLE bestellung (
+    id INT IDENTITY,
+    datum DATE NOT NULL,
+    summe MONEY NOT NULL,
+    kunde_id INT NOT NULL,
+    CONSTRAINT pk_bestellung 
+        PRIMARY KEY(id),
+    CONSTRAINT fk_bestellung_kunde 
+        FOREIGN KEY(kunde_id) 
+        REFERENCES kunde(id)
+);
+
+GO
---
+++ sql/DML/datainsert.sql
@@ -0,0 +1,17 @@
+USE DMLbeispiel
+
+--neue datensätze anlegen
+
+INSERT INTO kunde
+VALUES (1, N'Max', N'Mustermann', '19900404');
+
+-- Das N vor Max und Mustermann ist eine markierung für ein "N"VARCHAR bei zeichen außerhalb dem ASCII bereich
+-- im datum kann auch 1990-04-04 funktionieren
+
+INSERT INTO kunde (id, vorname, name)
+VALUES (2, N'Mia' , N'Musterfrau')
+--verkürzte schreibweise ohne datum
+
+SELECT *
+FROM kunde;
+
